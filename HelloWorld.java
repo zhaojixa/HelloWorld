984f3aa680c5b08631b1fa5e6b1c1857cb2fbe3a
zhaojixa <41097945+zhaojixa@users.noreply.github.com>
2018-09-17 00:05:06 +0800

    HelloWorld
    HelloWorld test!!

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+    /**
+
+    * 输出一行字符串“Hello World!”
+
+    * @param args
+
+    */
+
+    public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+
+    }
+
+}
